410133c33b694a7eaff3d568697799c4335afe4e
Mike Chadwick <michael.chadwick@ao.com>
2017-09-30 15:53:59 +0100

    picture tests started

---
+++ __tests__/lazy-picture.test.js
@@ -0,0 +1,19 @@
+const { LazyLoad } = require('../js/lazy');
+
+const {
+	createLazyImage,
+	createLazyBackground,
+	cleanUpDom,
+	lazyClassNames,
+	lazyImagePaths
+} = require('./test-helpers');
+
+describe('LazyScroll class tests', function() {
+
+	afterEach(cleanUpDom);
+
+  it('Should work', function() {
+    
+  })
+
+});
