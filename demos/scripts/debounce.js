e6fcb6ad2dbdce1fa1d10eeda68c6b79db10de87
James Almond <jamestalmond@gmail.com>
2017-11-16 11:48:15 +0000

    Lazy demos

---
+++ demos/scripts/debounce.js
@@ -0,0 +1,30 @@
+// taken from http://underscorejs.org/#debounce
+function debounce(callback, wait, immediate) {
+
+  let timeout;
+
+  return function bounce() {
+
+    const context = this;
+    const args = arguments;
+
+    function later() {
+			timeout = null;
+			if(!immediate) {
+        callback.apply(context, args);
+      }
+    }
+
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+
+    if(callNow) {
+      callback.apply(context, args);
+    }
+
+  }
+
+}
+
+module.exports = { debounce };
