fafd38269084eafe5b5c8d779e8108731bdf2579
Alastair Brown <alastair.brown@ao.com>
2018-03-19 11:01:29 +0000

    Update to Webpack4.Also added `lazy-all.min.js` so ustilites aren't included twice.

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,42 @@
 const path = require('path');
 const webpack = require('webpack');
 const isProduction = process.env.NODE_ENV === 'production';
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin;
 
-const webpackPlugins = isProduction ? [
-	// Production plugins
-	new webpack.DefinePlugin({
-		'process.env.NODE_ENV': '"production"'
-	}),
-	new webpack.optimize.UglifyJsPlugin(),
-	new webpack.optimize.ModuleConcatenationPlugin(),
-	new BundleAnalyzerPlugin()
-] : [
-	 // Development plugins
-];
-
-module.exports = {
-	entry: {
-		'lazy.min': path.resolve(__dirname, 'js/lazy.js'),
-		'lazy-scroll.min': path.resolve(__dirname, 'js/lazy-scroll.js'),
-		'lazy-proximity.min': path.resolve(__dirname, 'js/lazy-proximity.js')
-	},
-	output: {
-		path: path.resolve(__dirname, 'js/min'),
-		filename: '[name].js',
-		libraryTarget: 'umd'
-	},
-	resolve: {
-		modules: [
-			path.join(__dirname, 'js'),
-			'node_modules'
-		],
-		extensions: ['.js']
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader'
-			}
-		}]
-	},
-	plugins: webpackPlugins,
-	watch: !isProduction
+module.exports = function(env, argv) {
+	const webpackPlugins =
+		argv.mode === 'production' ? [new BundleAnalyzerPlugin()] : [];
+	return {
+		entry: {
+			'lazy.min': path.resolve(__dirname, 'js/lazy.js'),
+			'lazy-scroll.min': path.resolve(__dirname, 'js/lazy-scroll.js'),
+			'lazy-proximity.min': path.resolve(__dirname, 'js/lazy-proximity.js'),
+			'lazy-all.min': [
+				path.resolve(__dirname, 'js/lazy-scroll.js'),
+				path.resolve(__dirname, 'js/lazy-proximity.js')
+			]
+		},
+		output: {
+			path: path.resolve(__dirname, 'js/min'),
+			filename: '[name].js',
+			libraryTarget: 'umd'
+		},
+		resolve: {
+			modules: [path.join(__dirname, 'js'), 'node_modules'],
+			extensions: ['.js']
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader'
+					}
+				}
+			]
+		},
+		plugins: webpackPlugins
+	};
 };
